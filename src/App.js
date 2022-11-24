53d4e181ac3e425c80b0c9906716ef0b89829e69
unknown <justiiturraspe@gmail.com>
2022-11-24 04:26:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href='https://drive.google.com/file/d/1p8YhZ3q-Ni5WcUYJCxAni0ada_ss2KS9/view' target="_blank" style={{textDecoration:'none'}}>
+        <h1 className='title' >          Justina Iturraspe        </h1>  
+      </a>
+      
+      < Profile />
+      < Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import DoneIcon from '@mui/icons-material/Done';
+import './Footer.css'
+
+function Footer() {
+
+  return (
+    <div className='socialMedia'>
+      <a href='https://github.com/justinait' target="_blank">
+        <GitHubIcon className='socialMediaIcon' fontSize='large' />
+      </a>
+
+      <a href='https://www.linkedin.com/in/justinaiturraspe/' target="_blank">
+        <LinkedInIcon className='socialMediaIcon' fontSize='large' />
+      </a>
+      
+      <EmailIcon 
+        onClick={() => {          navigator.clipboard.writeText('justiiturraspe@gmail.com')        }}
+        className='socialMediaIcon'
+        fontSize='large'
+      />
+        
+    </div>
+  )
+}
+
+export default Footer;
