51780903c4c26e5038614ece6a878b538a3cf700
justinait <justiiturraspe@gmail.com>
2023-08-10 17:03:47 -0300

    cursor w pointer

--- src/App.js
+++ src/App.js
@@ -6,24 +6,28 @@ import Header from './components/Header/Header';
 import Education from './components/Education/Education';
 import Skills from './components/Skills/Skills';
 import Home from './components/Home/Home';
+import { useState } from 'react';
 
 function App() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-      <BrowserRouter>
-      
+    <BrowserRouter>
+      <div onMouseMove={handleMouseMove} className="app-container">
         <Header />
-      
         <Routes>
-
-          <Route path='/' element={< Home />} />
-          
-          <Route path='/education' element={< Education />} />
-          <Route path='/projects' element={< Projects />} />
-          <Route path='/skills' element={< Skills />} />
-
+          <Route path='/' element={<Home />} className='portfolio-item'/>
+          <Route path='/education' element={<Education />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/skills' element={<Skills />} />
         </Routes>
-        
-      </BrowserRouter>
+        <div className="cursor" style={{ left: cursorPosition.x, top: cursorPosition.y }}></div>
+      </div>
+    </BrowserRouter>
   );
 }
 
