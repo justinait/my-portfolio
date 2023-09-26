035089102cf0639e62f8269d8ed5badb02699699
justinait <justiiturraspe@gmail.com>
2023-09-26 16:04:12 -0300

    education and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,20 +2,26 @@ import React from 'react'
 import Education from '../Education/Education'
 import Skills from '../Skills/Skills'
 import './About.css'
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import Face4Icon from '@mui/icons-material/Face4';
 
 function About() {
   return (
     <div>
-        <p className='aboutText'>
-            Soy Justina, estudiante autodidacta de Desarrollo Web enfocado en Frontend. 
-            Actualmente estoy potenciando mi desarrollo
-            académico y profesional como desarrolladora web
-            de manera freelance. Con una actitud entusiasta y
-            una pasión por aprender, me caracterizo por asumir
-            nuevos desafíos y enfrentarlos con determinación.        
+      <div className='aboutText'>
+        <p className='subtitles'>Sobre mí</p>
+        <p >
+          Soy Justina, estudiante autodidacta de Desarrollo Web enfocado en Frontend. 
+          Actualmente estoy potenciando mi desarrollo
+          académico y profesional como desarrolladora web
+          freelance. Con una actitud entusiasta y
+          una pasión por aprender, me caracterizo por asumir
+          nuevos desafíos y enfrentarlos con determinación.       
         </p>
-        <Education />
-        <Skills />
+      </div>
+      
+      <Education />
+      <Skills />
     </div>
   )
 }
--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import './Education.css'
+import SchoolIcon from '@mui/icons-material/School';
 
 function Education() {
   return (
     <div className='educationContainer'>
-      <div className='education'>
-        <img src='/logos/UADER2.png' className='educationIcon'/>  
-        {/* <h4>UADER</h4> */}
-        <h5>Introducción al Diseño UX/UI</h5>
-        <p>2023</p>
-      </div>
-      <div className='education'>
-        <img src='/logos/coderhouse.png' className='educationIcon'/>  
-        {/* <h4>Coder House</h4> */}
-        <h5>React JS</h5>
-        <p>2022</p>
-      </div>
-      <div className='education'>
-        <img src='/logos/dh.jpg' className='educationIcon'/>
-        {/* <h4>Digital House</h4> */}
-        <h5>Full Stack Developer</h5>
-        <p>2021</p>
-      </div>
+      <p className='subtitles'>        Educación       </p>
       
-      <div className='education'>
-        <img src='/logos/unl.gif' className='educationIcon'/>  
-        {/* <h4>Universidad Nacional del Litoral</h4> */}
-        <h5>Analista en Sistemas</h5>
-        <p>2019 - 2021</p>
-      </div>    
+      <div className='timeline'>
+        <div className='education'>
+          {/* <img src='/logos/UADER2.png' className='educationIcon'/>   */}
+          <h4>Universidad Autónoma De Entre Ríos</h4>
+          <h5>Introducción al Diseño UX/UI</h5>
+          <p className='year'>2023</p>
+        </div>
+        <div className='education'>
+          {/* <img src='/logos/coderhouse.png' className='educationIcon'/>   */}
+          <h4>Coder House</h4>
+          <h5>Curso - React JS</h5>
+          <p className='year'>2022</p>
+        </div>
+        <div className='education'>
+          {/* <img src='/logos/dh.jpg' className='educationIcon'/> */}
+          <h4>Digital House</h4>
+          <h5>Curso - Full Stack Developer</h5>
+          {/* <br/> */}
+          <p className='year'>2021</p>
+        </div>
+        
+        <div className='education'>
+          {/* <img src='/logos/unl.gif' className='educationIcon'/>   */}
+          <h4>Universidad Nacional del Litoral</h4>
+          <h5>Analista en Sistemas</h5>
+          <p className='year'>2019 - 2021</p>
+        </div>    
+      </div>
     </div>
   )
 }
