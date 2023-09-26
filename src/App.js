05e76168dd7b6a890b05ee2c33dd483d1cd4de48
justinait <justiiturraspe@gmail.com>
2023-09-26 13:53:43 -0300

    CAMBIOS MODERNOS

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Projects from './components/Projects/Projects';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import { useState } from 'react';
 import About from './components/About/About';
 
 function App() {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-
-  const handleMouseMove = (e) => {
-    setCursorPosition({ x: e.clientX, y: e.clientY });
-  };
-
   return (
     <BrowserRouter>
-      <div onMouseMove={handleMouseMove} className="app-container">
+      <div className="app-container">
         <Header />
         <Routes>
           
@@ -27,7 +20,6 @@ function App() {
         </Routes>
         
       {/* <Footer/> */}
-        <div className="cursor" style={{ left: cursorPosition.x, top: cursorPosition.y }}></div>
       </div>
     </BrowserRouter>
   );
--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -4,12 +4,11 @@ import './Education.css'
 function Education() {
   return (
     <div className='educationContainer'>
-      
       <div className='education'>
-        <img src='/logos/dh.jpg' className='educationIcon'/>
-        {/* <h4>Digital House</h4> */}
-        <h5>Full Stack Developer</h5>
-        <p>2021</p>
+        <img src='/logos/UADER2.png' className='educationIcon'/>  
+        {/* <h4>UADER</h4> */}
+        <h5>Introducción al Diseño UX/UI</h5>
+        <p>2023</p>
       </div>
       <div className='education'>
         <img src='/logos/coderhouse.png' className='educationIcon'/>  
@@ -17,6 +16,13 @@ function Education() {
         <h5>React JS</h5>
         <p>2022</p>
       </div>
+      <div className='education'>
+        <img src='/logos/dh.jpg' className='educationIcon'/>
+        {/* <h4>Digital House</h4> */}
+        <h5>Full Stack Developer</h5>
+        <p>2021</p>
+      </div>
+      
       <div className='education'>
         <img src='/logos/unl.gif' className='educationIcon'/>  
         {/* <h4>Universidad Nacional del Litoral</h4> */}
