1b688d94192744baf6aa926fe540295242f6df3c
justinait <justiiturraspe@gmail.com>
2023-10-03 12:52:18 -0300

    contact

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,17 @@ import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import './Footer.css'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 function Footer() {
-
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Copiar Email
+    </Tooltip>
+  );
   return (
     <div className='socialMedia'>
       <a href='https://github.com/justinait' target="_blank">
@@ -16,11 +23,21 @@ function Footer() {
         <LinkedInIcon className='socialMediaIcon' fontSize='large' />
       </a>
       
-      <EmailIcon 
-        onClick={() => {          navigator.clipboard.writeText('justiiturraspe@gmail.com')        }}
-        className='socialMediaIcon'
-        fontSize='large'
-      />
+      <OverlayTrigger
+        placement="right"
+        // delay={{ show: 250, hide: 400 }}
+        overlay={renderTooltip}
+      >
+        <EmailIcon 
+          onClick={() => {          navigator.clipboard.writeText('justiiturraspe@gmail.com')        }}
+          className='socialMediaIcon'
+          fontSize='large'
+        />
+      </OverlayTrigger>
+      
+      <a href='wa.link/jy3kda' target="_blank">
+        <WhatsAppIcon className='socialMediaIcon' fontSize='large' />
+      </a>
         
     </div>
   )
