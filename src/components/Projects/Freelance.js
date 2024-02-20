f57be6844234b3cb3bc55311e37c09422003ace9
justinait <justiiturraspe@gmail.com>
2024-02-20 09:59:42 -0300

    correct

--- src/components/Projects/Freelance.js
+++ src/components/Projects/Freelance.js
@@ -6,7 +6,7 @@ function Freelance() {
     const freelanceProjects = [
         { name: 'RollerBeige', image: '/projects/roller web.png', web: 'https://rollerbeige.com.ar', description: 'Maquetación y Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'Auth', 'GitHub', 'Firebase database'] },
         { name: 'CM Broker', image: '/projects/cmbrokerweb1.png', web: 'https://cmbroker.com.ar/', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'Auth', 'FormsJs', 'GitHub'] },
-        { name: 'CAI Ventures', image: '/projects/cai ventures web.png', web: 'https://www.caiventuresconsultoria.com', description: 'Equipo de desarrollo. 4 desarrolladores.', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'Firebase database', 'FormsJs', 'GitHub', 'Git Flow'] },
+        { name: 'CAI Ventures', image: '/projects/cai ventures web.png', web: 'https://www.caiventuresconsultoria.com', description: 'Equipo de desarrollo. 4 desarrolladores.', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'FormsJs', 'GitHub', 'Git Flow'] },
         { name: 'Eskamaq', image: '/projects/eskamaq web.png', web: 'https://www.eskamaq.com', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub', 'Firebase database'] },
         { name: 'Jorgelina Parkinson', image: '/projects/parkinson web.png', web: 'https://www.jorgelinaparkinson.com/', description: 'Maquetación y Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'FormsJs', 'GitHub', 'Firebase database'] },
         { name: 'Regia', image: '/projects/regia web.png', web: 'https://www.regia.ar/', description: 'Desarrollo', stack: ['Javascript', 'HTML', 'CSS', 'GitHub'] },
