022f634d414b866f550b859b5abdff46ecb6cef9
justinait <justiiturraspe@gmail.com>
2024-09-09 17:20:19 -0300

    polo

--- src/components/Projects/Freelance.js
+++ src/components/Projects/Freelance.js
@@ -3,10 +3,10 @@ import './Freelance.css'
 
 function Freelance() {
     const freelanceProjects = [
-        { name: 'Autos Polo', image: '/projects/autospolo web.png', web: 'https://www.autospolo.com', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub', 'Maquetación'] },
+        { name: 'Autos Polo', image: '/projects/autospolo web.png', web: 'https://www.autospolo.com', description: 'Maquetación y Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub', 'Diseño UX UI', 'Firebase database', 'Auth'] },
         { name: 'The Looney Studio', image: '/projects/looney web.png', web: 'https://www.thelooneystudio.com', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub'] },
         { name: 'Medical City', image: '/projects/medicalcity web.png', web: 'https://medicalcitypv.com', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub'] },
-        { name: 'Eleven Pro Cars', image: '/projects/elevenpro web.png', web: 'https://elevenprocar.com', description: 'Maquetación y Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub'] },
+        { name: 'Eleven Pro Cars', image: '/projects/elevenpro web.png', web: 'https://elevenprocar.com', description: 'Maquetación y Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'GitHub', 'Firebase database'] },
         { name: 'ParmaShop', image: '/projects/parmaweb.png', web: 'https://parmashop.com.ar', description: 'Maquetación y Desarrollo', stack: ['React Js', 'E-commerce', 'Javascript', 'HTML', 'CSS', 'Auth', 'GitHub', 'Firebase database', 'Mercado Pago'] },
         { name: 'RollerBeige', image: '/projects/rollertienda web.png', web: 'https://rollerbeige.com.ar', description: 'Maquetación y Desarrollo', stack: ['React Js', 'E-commerce','Javascript', 'HTML', 'CSS', 'Auth', 'GitHub', 'Firebase database', 'Mercado Pago'] },
         { name: 'CM Broker', image: '/projects/cmbrokerweb1.png', web: 'https://cmbroker.com.ar/', description: 'Desarrollo', stack: ['React Js', 'Javascript', 'HTML', 'CSS', 'FormsJs', 'GitHub'] },
